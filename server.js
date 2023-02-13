49682001ad8ea7a46f6b13a96b632f097a5fb03c
tropical9 <113566952+tropical9@users.noreply.github.com>
2023-02-12 21:52:34 -0800

    server

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
 const path = require('path');
 const express = require('express');
 const sequelize = require('./config/connection');
-const expressHandlebars = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const session = require('express-session');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
+const hbs = exphbs.create({ helpers });
 
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess  = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+}
+
 const handlebars = exphbs.create({ helpers });
 
-
+app.use (session(sess));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
+  
 
 
