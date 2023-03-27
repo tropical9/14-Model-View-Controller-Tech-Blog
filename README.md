b7f93d27642eac95c437281cbffde21e5550ff41
tropical9 <113566952+tropical9@users.noreply.github.com>
2023-03-26 22:17:25 -0700

    folders

--- README.md
+++ README.md
@@ -3,10 +3,13 @@ Writing about tech can be just as important as making it. Developers spend plent
 
 Your challenge this week is to build a CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well. You’ll build this site completely from scratch and deploy it to Heroku. Your app will follow the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication.
 
+```
 User Story
 AS A developer who writes about tech
 I WANT a CMS-style blog site
 SO THAT I can publish articles, blog posts, and my thoughts and opinions
+```
+```
 Acceptance Criteria
 GIVEN a CMS-style blog site
 WHEN I visit the site for the first time
@@ -41,4 +44,4 @@ WHEN I click on the logout option in the navigation
 THEN I am signed out of the site
 WHEN I am idle on the site for more than a set time
 THEN I am able to view comments but I am prompted to log in again before I can add, update, or delete comments
-Mock-Up
+```
---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require ('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+//create User model
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING, 
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return new UserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        }
+    }
+)
