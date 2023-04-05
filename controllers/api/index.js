c19c3a22c70c38b2d3531ca6ed19f48c1f6bf6ec
tropical9 <113566952+tropical9@users.noreply.github.com>
2023-04-04 23:48:13 -0700

    login route working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const postRoutes = require('./postRoutes');
 const userRoutes = require('./userRoutes');
+const loginRoutes = require('./loginRoutes');
 
 router.use('/users', userRoutes);
 router.use('/post', postRoutes);
+router.use('/login', loginRoutes);
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,6 +26,55 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/dashboard', async (req, res) => {
+    try {
+        //get all posts and JOIN with user data
+        const postData = await Post.findAll({
+            include: [
+                { 
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.get ({ plain: true }));
+        //pass serialized data and session flag into template
+        res.render('dashboard', {
+             posts,
+             logged_in: req.session.logged_in
+        
+         });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/dashboard', async (req, res) => {
+    try {
+        //get all posts and JOIN with user data
+        const postData = await Post.findAll({
+            include: [
+                { 
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.get ({ plain: true }));
+        //pass serialized data and session flag into template
+        res.render('homepage', {
+             posts,
+             logged_in: req.session.logged_in
+        
+         });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
 // //use with auth middleware to prevent access to route
 // router.get ('/user', withAuth, async (req, res) => {
 //     try {
@@ -46,15 +95,15 @@ router.get('/', async (req, res) => {
 //     }
 // });
 
-// //login 
-// router.get('/login', (req, res) => {
-//     if (req.session.loggedIn) {
-//         res.redirect ('/profile');
-//         return;
-//     }
+//login 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect ('/profile');
+        return;
+    }
 
-//     res.render('login');
-// });
+    res.render('login');
+});
 
 module.exports = router;
 
